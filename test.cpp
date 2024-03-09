608c533faedef2d2946a201fc72fd06b705d0206
Kaleidopink <lunar_ubuntu@qq.com>
2024-03-09 10:39:01 +0800

    first commit

---
+++ test.cpp
@@ -0,0 +1,9 @@
+// Date:   Sat Mar 09 10:38:27 2024
+// Mail:   lunar_ubuntu@qq.com
+// Author: https://github.com/xiaoqixian
+
+#include <stdio.h>
+
+int main() {
+    printf("this is a test\n");
+}
